5758a66de03836f45528aa5c2868a78e244e17cb
alien190 <alien1900@bk.ru>
2018-10-09 13:50:11 +0700

    added eyes animation

--- app/src/main/java/com/example/alien/course05task06/MainActivity.java
+++ app/src/main/java/com/example/alien/course05task06/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.alien.course05task06;
 
-import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
+import android.support.graphics.drawable.Animatable2Compat;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,30 +11,46 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
+    private ImageView mImageViewEyes;
+    private AnimatedVectorDrawableCompat mEyesAnimation;
+    private AnimatedVectorDrawableCompat mHeadAndHandsAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEyesAnimation = AnimatedVectorDrawableCompat.create(this, R.drawable.animated_logo_eyes);
+        mHeadAndHandsAnimation = AnimatedVectorDrawableCompat.create(this, R.drawable.animated_logo);
         mImageView = findViewById(R.id.image);
+        mImageView.setImageDrawable(mHeadAndHandsAnimation);
+        mImageViewEyes = findViewById(R.id.image_eyes);
+        mImageViewEyes.setImageDrawable(mEyesAnimation);
+        mHeadAndHandsAnimation.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                mEyesAnimation.start();
+                super.onAnimationEnd(drawable);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mImageView.setOnClickListener(this::startAnimation);
+        mImageViewEyes.setOnClickListener(this::startAnimation);
     }
 
     @Override
     protected void onPause() {
         mImageView.setOnClickListener(null);
+        mImageViewEyes.setOnClickListener(null);
         super.onPause();
     }
 
     private void startAnimation(View view) {
-        Drawable drawable = mImageView.getDrawable();
-        if (drawable instanceof Animatable) {
-            ((Animatable) drawable).start();
-        }
+        mHeadAndHandsAnimation.start();
     }
+
+
 }
